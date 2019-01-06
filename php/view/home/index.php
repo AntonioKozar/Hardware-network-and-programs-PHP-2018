4279bd427ae674925dc7a4a90e90fabf2515dc14
Antonio Kožar <08akozar@gmail.com>
2019-01-06 15:23:53 +0100

    06.01.2019.

---
+++ php/view/home/index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="../res/sticky-footer-navbar.css" rel="stylesheet">
+    
+</head>
+<body>
+    
+</body>
+</html>
